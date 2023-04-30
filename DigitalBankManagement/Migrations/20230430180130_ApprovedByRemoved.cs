a002401a078e5471aca08f225f060232a13935a9
Allen Lewis <allenlewis32@gmail.com>
2023-04-30 23:32:16 +0530

    ApprovedBy field removed

---
+++ DigitalBankManagement/Migrations/20230430180130_ApprovedByRemoved.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DigitalBankManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApprovedByRemoved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LoanApplications_Users_ApprovedBy",
+                table: "LoanApplications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LoanApplications_ApprovedBy",
+                table: "LoanApplications");
+
+            migrationBuilder.DropColumn(
+                name: "ApprovedBy",
+                table: "LoanApplications");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ApprovedBy",
+                table: "LoanApplications",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LoanApplications_ApprovedBy",
+                table: "LoanApplications",
+                column: "ApprovedBy");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LoanApplications_Users_ApprovedBy",
+                table: "LoanApplications",
+                column: "ApprovedBy",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
