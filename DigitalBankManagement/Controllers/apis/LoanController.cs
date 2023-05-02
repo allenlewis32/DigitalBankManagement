2db91e546c0eb72f5f0e53c83057c8238c26cbd4
Allen Lewis <allenlewis32@gmail.com>
2023-05-02 09:36:00 +0530

    Approve method implemented

--- DigitalBankManagement/Controllers/apis/LoanController.cs
+++ DigitalBankManagement/Controllers/apis/LoanController.cs
@@ -1,4 +1,5 @@
-﻿using DigitalBankManagement.Data;
+﻿using System.ComponentModel.DataAnnotations;
+using DigitalBankManagement.Data;
 using DigitalBankManagement.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,25 +18,80 @@ namespace DigitalBankManagement.Controllers
 		}
 
 		[HttpGet]
-		[ResponseCache(Duration =0)]
 		public IActionResult Get([FromHeader] string sessionId)
 		{
-			UserModel? user = Helper.GetUser(_context, sessionId);
-			if (user == null)
+			try
 			{
-				return Unauthorized();
-			}
-			IQueryable<LoanApplicationModel> applications = _context.LoanApplications;
-			if (user.Role.Name.ToLower() == "user")
-			{
-				applications = applications.Where(application => application.UserId == user.Id);
-				// don't send user information loaded while using where clause
-				applications.ForEachAsync(application =>
+				UserModel? user = Helper.GetUser(_context, sessionId);
+				if (user == null)
 				{
-					application.User = null;
-				}).Wait();
+					return Unauthorized();
+				}
+				IQueryable<LoanApplicationModel> applications = _context.LoanApplications;
+				if (user.Role.Name.ToLower() == "user")
+				{
+					applications = applications.Where(application => application.UserId == user.Id);
+					// don't send user information loaded while using where clause
+					applications.ForEachAsync(application =>
+					{
+						application.User = null;
+					}).Wait();
+				}
+				return Ok(applications);
 			}
-			return Ok(applications);
+			catch
+			{
+				return Problem();
+			}
+		}
+
+		[HttpPost]
+		[Route("Approve")]
+		public IActionResult Approve([FromHeader] string sessionId, [FromForm] int applicationId)
+		{
+			try
+			{
+				UserModel? user = Helper.GetUser(_context, sessionId);
+				if (user == null || user.Role.Name == "user")
+				{
+					return Unauthorized();
+				}
+				LoanApplicationModel application = _context.LoanApplications.First(la => la.Id == applicationId);
+				application.Status = 1;
+				AccountModel account = new()
+				{
+					UserId = user.Id,
+					Amount = application.Amount,
+					DateCreated = DateTime.UtcNow,
+					Active = true,
+					Type = 1,
+				};
+				LoanModel loan = new()
+				{
+					Account = account,
+					Duration = application.Duration,
+					DebitFrom = application.DebitFrom,
+				};
+				CalculateEmi(loan);
+				_context.Loans.Add(loan);
+				_context.SaveChanges();
+				return Ok();
+			}
+			catch
+			{
+				return Problem();
+			}
+		}
+
+		// calculates Emi for loan
+		private void CalculateEmi(LoanModel loan)
+		{
+			decimal P = loan.Account.Amount;
+			decimal R = _context.Interests.First().Loan;
+			int N = loan.Duration * 12; // convert years to months
+			decimal t = (decimal)Math.Pow((double)(1 + R / 1200), N);
+			decimal emi = P * R * t / (t - 1);
+			loan.Emi = emi;
 		}
 	}
 }
