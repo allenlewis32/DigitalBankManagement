a1b1f6cdd59049936aea89c881f7227cb1731958
Allen Lewis <allenlewis32@gmail.com>
2023-05-02 11:59:58 +0530

    Get managers implemented

---
+++ DigitalBankManagement/Controllers/apis/ManagerController.cs
@@ -0,0 +1,53 @@
+﻿using DigitalBankManagement.Data;
+using DigitalBankManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalBankManagement.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ManagerController : ControllerBase
+	{
+		private readonly ApplicationDbContext _context;
+		public ManagerController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public IActionResult Get([FromHeader] string sessionId)
+		{
+			try
+			{
+				var user = Helper.GetUser(_context, sessionId);
+				if(user == null || user.Role.Name != "admin")
+				{
+					return Unauthorized();
+				}
+				var managers = new List<UserModel>();
+				_context.Users.Include(user => user.Role)
+					.Where(user => user.Active && user.Role.Name.Equals("manager"))
+					.ForEachAsync(user =>
+					{
+						// create new manager classes as we shouldn't be returning sensitive information
+						managers.Add(new()
+						{
+							Id = user.Id,
+							Email = user.Email,
+							FirstName = user.FirstName,
+							LastName = user.LastName,
+							Active = true,
+						});
+					})
+					.Wait();
+				return Ok(managers);
+			}
+			catch
+			{
+				return Problem();
+			}
+		}
+	}
+}
