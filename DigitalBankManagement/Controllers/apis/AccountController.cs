5def0f109248f8723e311250297e2158dea2711f
Allen Lewis <allenlewis32@gmail.com>
2023-05-02 11:46:28 +0530

    Get accounts implemented

--- DigitalBankManagement/Controllers/apis/AccountController.cs
+++ DigitalBankManagement/Controllers/apis/AccountController.cs
@@ -26,28 +26,30 @@ namespace DigitalBankManagement.Controllers
 				{
 					return Unauthorized();
 				}
-				var res = new Dictionary<string, string>();
+				var res = new Dictionary<string, object>();
 				switch (user.Role.Name)
 				{
 					case "admin":
 						res["savings"] = _context.Accounts
-							.Count(account => account.Type == AccountModel.TypeSavings).ToString();
+							.Count(account => account.Type == AccountModel.TypeSavings);
 						res["fd"] = _context.Accounts
-							.Count(account => account.Type == AccountModel.TypeFD).ToString();
+							.Count(account => account.Type == AccountModel.TypeFD);
 						res["rd"] = _context.Accounts
-							.Count(account => account.Type == AccountModel.TypeRD).ToString();
+							.Count(account => account.Type == AccountModel.TypeRD);
 						res["loan"] = _context.Accounts
-							.Count(account => account.Type == AccountModel.TypeLoan).ToString();
+							.Count(account => account.Type == AccountModel.TypeLoan);
 						res["managers"] = _context.Users
 							.Count(user =>
-							user.RoleId == _context.Roles.First(role => role.Name == "manager").Id)
-							.ToString();
+							user.RoleId == _context.Roles.First(role => role.Name == "manager").Id);
 						res["users"] = _context.Users
 							.Count(user =>
-							user.RoleId == _context.Roles.First(role => role.Name == "user").Id)
-							.ToString();
+							user.RoleId == _context.Roles.First(role => role.Name == "user").Id);
 						break;
 					case "user":
+						res["savings"] = _context.Accounts.Where(account => account.Type == AccountModel.TypeSavings).ToList();
+						res["fd"] = _context.Accounts.Where(account => account.Type == AccountModel.TypeFD).ToList();
+						res["rd"] = _context.RdAccounts.Include(rd => rd.Account).ToList();
+						res["loan"] = _context.Loans.Include(loan => loan.Account).ToList();
 						break;
 				}
 				return Ok(res);
