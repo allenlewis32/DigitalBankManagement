7c3435fabc4f19dba0bc9d9c70b45f92251b0977
Allen Lewis <allenlewis32@gmail.com>
2023-05-02 10:59:37 +0530

    Get active loans

---
+++ DigitalBankManagement/Controllers/apis/LoanController.cs
@@ -0,0 +1,49 @@
+﻿using DigitalBankManagement.Data;
+using DigitalBankManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalBankManagement.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LoanController : ControllerBase
+	{
+		private readonly ApplicationDbContext _context;
+		public LoanController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public IActionResult Get([FromHeader] string sessionId)
+		{
+			try
+			{
+				UserModel? user = Helper.GetUser(_context, sessionId);
+				if (user == null)
+				{
+					return Unauthorized();
+				}
+				IQueryable<LoanModel> loans = _context.Loans.Include(loan => loan.Account)
+					.Where(loan => loan.Account.Active);
+				if (user.Role.Name.ToLower() != "admin")
+				{
+					loans = loans.Where(loan => loan.Account.UserId == user.Id);
+				}
+
+				// don't send user information
+				loans.ForEachAsync(loan =>
+				{
+					loan.Account.User = null;
+				}).Wait();
+				return Ok(loans);
+			}
+			catch
+			{
+				return Problem();
+			}
+		}
+	}
+}
