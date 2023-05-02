bf7e4ce895fe4394336a99490f0a6a12ce09d66a
Allen Lewis <allenlewis32@gmail.com>
2023-05-02 14:28:49 +0530

    CreateAccountModel added

---
+++ DigitalBankManagement/Models/CreateAccountModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DigitalBankManagement.Models
+{
+	public class CreateAccountModel
+	{
+		public int Type {  get; set; }
+		public int? DebitFrom { get; set; }
+		public int? Duration { get; set; }
+		public decimal? MonthlyDeposit { get; set; }
+		public decimal? InitialDeposit { get; set; }
+	}
+}
