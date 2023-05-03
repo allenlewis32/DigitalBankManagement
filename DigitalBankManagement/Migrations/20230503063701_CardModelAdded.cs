e70783b14f1f362ed91c16721a27d570c6e22b5f
Allen Lewis <allenlewis32@gmail.com>
2023-05-03 12:09:41 +0530

    CardModel added

---
+++ DigitalBankManagement/Migrations/20230503063701_CardModelAdded.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DigitalBankManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class CardModelAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Id = table.Column<decimal>(type: "decimal(16,0)", nullable: false)
+                        .Annotation("SqlServer:Identity", "4000000000000000,1"),
+                    AccountId = table.Column<int>(type: "int", nullable: true),
+                    Pin = table.Column<int>(type: "int", nullable: false),
+                    Expiry = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cards_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_AccountId",
+                table: "Cards",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+        }
+    }
+}
---
+++ DigitalBankManagement/Models/CardModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DigitalBankManagement.Models
+{
+	public class CardModel
+	{
+		[Key]
+		[Column(TypeName = "decimal(16)")]
+		public decimal Id { get; set; }
+		public int? AccountId { get; set; }
+		public AccountModel? Account { get; set; }
+		public int Pin { get; set; }
+		public DateTime Expiry { get; set; }
+	}
+}
