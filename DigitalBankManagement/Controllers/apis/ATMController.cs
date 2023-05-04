6a8a66a5c9f4823128167f405f20c4fbfdb2f608
Allen Lewis <allenlewis32@gmail.com>
2023-05-04 09:41:36 +0530

    ATM methods implemented

---
+++ DigitalBankManagement/Controllers/apis/ATMController.cs
@@ -0,0 +1,70 @@
+﻿using System.Net.NetworkInformation;
+using DigitalBankManagement.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalBankManagement.Controllers.apis
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ATMController : ControllerBase
+	{
+		private readonly ApplicationDbContext _context;
+		public ATMController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost]
+		[Route("Credit")]
+		public IActionResult Credit([FromForm] string cardNumber, [FromForm] decimal amount, [FromForm] int pin)
+		{
+			try
+			{
+				// verify card
+				decimal cardId = decimal.Parse(cardNumber);
+				var card = _context.Cards.Include(card => card.Account).FirstOrDefault(card => card.Id == cardId && card.Expiry > DateTime.UtcNow);
+				if (card == null)
+				{
+					return BadRequest();
+				}
+				if(card.Pin != pin)
+				{
+					return Unauthorized();
+				}
+
+				return Helper.TransferMoney(_context, this, null, amount, card.Account);
+			}
+			catch
+			{
+				return Problem();
+			}
+		}
+
+		[HttpPost]
+		[Route("Debit")]
+		public IActionResult Debit([FromForm] string cardNumber, [FromForm] decimal amount, [FromForm] int pin)
+		{
+			try
+			{
+				// verify card
+				decimal cardId = decimal.Parse(cardNumber);
+				var card = _context.Cards.Include(card => card.Account).FirstOrDefault(card => card.Id == cardId && card.Expiry > DateTime.UtcNow);
+				if (card == null)
+				{
+					return BadRequest();
+				}
+				if (card.Pin != pin)
+				{
+					return Unauthorized();
+				}
+
+				return Helper.TransferMoney(_context, this, card.Account, amount, null);
+			}
+			catch
+			{
+				return Problem();
+			}
+		}
+	}
+}
