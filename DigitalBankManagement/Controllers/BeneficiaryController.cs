f521262c477c06973106e115bd6835fb58f4b5c8
Allen Lewis <allenlewis32@gmail.com>
2023-05-08 16:18:10 +0530

    View beneficiaries implemented

---
+++ DigitalBankManagement/Controllers/BeneficiaryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalBankManagement.Controllers
+{
+	public class BeneficiaryController : Controller
+	{
+		[Route("/Beneficiary/Index", Name = "BeneficiaryIndex")]
+		public IActionResult Index()
+		{
+			try
+			{
+				dynamic? beneficiaries = Helper.Get(this, "Beneficiary", null, Request.Cookies["sessionId"], TempData);
+				ViewData["beneficiaries"] = beneficiaries;
+				return View();
+			}
+			catch // unauthorized
+			{
+				return RedirectToRoute("Login");
+			}
+		}
+	}
+}
